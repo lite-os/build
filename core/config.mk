b0b388f263c0c93730dc574c666ff993d30311a6
aleha-druga <aleha.druga@gmail.com>
2018-05-15 12:36:24 +0000

    rename track dir sepolicy

--- core/config.mk
+++ core/config.mk
@@ -969,7 +969,7 @@ endif
 ifneq ($(LITE_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/lite/sepolicy/common/sepolicy.mk)
+$(eval include device/lite/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
